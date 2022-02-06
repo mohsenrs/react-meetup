f92900e4a0a5e23c76e4cbdde6af44712708f692
mohsenrs <mohsen8022@gmail.com>
2022-02-06 18:52:56 +0330

    form

--- src/pages/NewMeetUp.js
+++ src/pages/NewMeetUp.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from 'react'
+import NewMeetupForm from './../components/meetups/NewMeetupForm';
 
 function NewMeetUp() {
-  return <div>NewMeetUp</div>;
+  return (
+    <section>
+      <h1>add new meetup</h1>
+      <NewMeetupForm />
+    </section>
+  )
 }
 
-export default NewMeetUp;
+export default NewMeetUp
